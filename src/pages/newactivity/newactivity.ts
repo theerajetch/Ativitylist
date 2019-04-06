4e47245a16bcc29e157aed0e3c7dd3070bdc800e
theerajet Chinlee <taonaja999@gmail.com>
2019-04-06 18:33:01 +0700

    add2

--- src/pages/newactivity/newactivity.ts
+++ src/pages/newactivity/newactivity.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
+
 /**
  * Generated class for the NewactivityPage page.
  *
@@ -14,12 +18,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'newactivity.html',
 })
 export class NewactivityPage {
+  title:any;
+  description:any;
+  addedToDo:any[]=[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage:Storage) {
+    this.storage=storage;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewactivityPage');
   }
-
+  saveRecords():void{
+    let todoObj={
+      title:"",
+      description:""
+    };
+    todoObj.title=this.title;
+    todoObj.description=this.description;
+    this.storage.get('todoDetails').then(
+      (val)=>{
+        if(val){
+          this.addedToDo=val;
+          this.addedToDo.push(todoObj);
+          this.storage.set('todoDetails', this.addedToDo);
+        }
+        else{
+          this.addedToDo.push(todoObj);
+          this.storage.set('todoDetails',this.addedToDo);
+        }
+      }
+    );
+    this.title="";
+    this.description="";
+    this.navCtrl.pop();
+  }
 }
